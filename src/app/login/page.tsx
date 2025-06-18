2fc220ce9ca675549935866f70a6cfcaa2c12bc9
Ryan Harris <ryan@synthesis.co.za>
2025-06-18 16:49:47 +0200

    learning nextjs

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Login() {
+  return (
+    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <div>
+        Login form goes here
+      </div>
+    </main>
+  );
+}
